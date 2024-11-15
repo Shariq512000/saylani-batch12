08ce495fd28ac449465a5a21cf588f3f1c89aef2
Meer Quais <meerquais017@gmail.com>
2024-11-15 10:49:09 +0500

    error handling

---
+++ javascript/try-catch-error-handling/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World Console 1")
+console.log("Hello World Console 2")
+console.log(userName);
+// try{
+//     console.log("Console in Try1")
+//     console.log("Console in Try2")
+// }catch(error){
+//     console.log("Console in Catch" , error)
+// }
+
+console.log("Hello World Console 3")
+console.log("Hello World Console 4")
+console.log("Hello World Console 5")
