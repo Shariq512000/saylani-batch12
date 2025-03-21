5cda9a0fb18cdec740e1ba0fd5db54bedd89a831
Muhammad Umair <muhammad.umair1019@gmail.com>
2025-03-21 08:47:24 +0500

    gsap basic and timeline

--- react/gsap-react/src/App.js
+++ react/gsap-react/src/App.js
@@ -5,6 +5,7 @@ import { useGSAP } from '@gsap/react';
 import { useRef, useState } from 'react';
 import CompFirst from './component/Comp1';
 import CompSecond from './component/Comp2';
+import Example from './component/Example';
 
 function App() {
 
@@ -15,10 +16,91 @@ function App() {
 
   // console.log("container" , container.current)
 
+  // useGSAP(() => {
+  //   console.log("position" , position)
+  //   gsap.to('.box', { x: position, delay: 1, duration: 2 });
+  // },{scope: container, dependencies: [position], revertOnUpdate: false});
+
   useGSAP(() => {
-    console.log("position" , position)
-    gsap.to('.box', { x: position, delay: 1, duration: 2 });
-  },{scope: container, dependencies: [position], revertOnUpdate: false});
+    // gsap.to("#box1" , {
+    //   x: 800,
+    //   duration: 2,
+    //   delay: 1
+    // })
+    // gsap.from("#box2" , {
+    //   x: 800,
+    //   duration: 2,
+    //   delay: 1,
+    //   backgroundColor: "blue",
+    // })
+
+    // gsap.from("h1",{
+    //   y: 30,
+    //   duration: 1,
+    //   delay: 1,
+    //   opacity: 0,
+    //   stagger: 0.3
+    // })
+
+    // gsap.to("#box1" , {
+    //   x: 800,
+    //   duration: 2,
+    //   delay: 1
+    // })
+
+    // gsap.to("#box2" , {
+    //   x: 800,
+    //   duration: 2,
+    //   delay: 3
+    // })
+
+    // gsap.to("#box3" , {
+    //   x: 800,
+    //   duration: 2,
+    //   delay: 5
+    // })
+
+    const tl = gsap.timeline();
+
+    tl.from(".logo", {
+      y:-30,
+      duration: 1,
+      opacity: 0,
+      delay:1
+    });
+
+    tl.from("nav li", {
+      y:-30,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.5
+    })
+
+    // tl.to("nav li", {
+    //   y:30,
+    //   duration: 1,
+    //   opacity: 0,
+    //   stagger: 0.5,
+    //   delay: 1
+    // })
+
+    // tl.to("#box1" , {
+    //   x: 800,
+    //   duration: 2,
+    //   delay: 1
+    // })
+
+    // tl.to("#box2" , {
+    //   x: 800,
+    //   duration: 2
+    // })
+
+    // tl.to("#box3" , {
+    //   x: 800,
+    //   duration: 2
+    // })
+
+  })
 
   const getRandomNumber = () => {
     let randomNumber = Math.random();
@@ -27,18 +109,42 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {/* <input type='text' onChange={(e) => {setPosition(e.target.value)}} />
-      <button>Done</button> */}
+    // <div className="App">
 
-      <button onClick={() => {setPosition(getRandomNumber())}}>Random Animate</button>
+    //   <button onClick={() => {setPosition(getRandomNumber())}}>Random Animate</button>
 
-      <div ref={container}>
-        <CompFirst />
-      </div>
-      <div>
-        <CompSecond />
-      </div>
+    //   <div ref={container}>
+    //     <CompFirst />
+    //   </div>
+    //   <div>
+    //     <CompSecond />
+    //   </div>
+    // </div>
+    <div>
+      {/* <div className='box' id='box1'></div>
+      <div className='box' id='box2'></div>
+      <div className='box' id='box3'></div> */}
+
+      {/* <header>
+        <div className='logo'>
+          Saylani
+        </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Gallery</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header> */}
+      <Example />
+
+      {/* <h1>HTML</h1>
+      <h1>CSS</h1>
+      <h1>JAVASCRIPT</h1>
+      <h1>REACT</h1> */}
+      
     </div>
   );
 }
---
+++ react/gsap-react/src/component/Example.jsx
@@ -0,0 +1,37 @@
+import { useGSAP } from '@gsap/react';
+import React from 'react';
+import gsap from 'gsap';
+
+const Example = () => {
+    useGSAP(() => {
+        gsap.from("#sec1 h1" , {
+            scale: 3,
+            duration: 1.5,
+            delay: 1,
+            opacity: 0
+        })
+        gsap.from("#sec2 h1" , {
+            scale: 3,
+            duration: 1.5,
+            delay: 1.5,
+            opacity: 0
+        })
+    })
+  return (
+    <>
+        <section id='sec1'>
+            <h1>Section 1</h1>
+        </section>
+
+        <section id='sec2'>
+            <h1>Section 2</h1>
+        </section>
+
+        <section id='sec3'>
+            <h1>Section 3</h1>
+        </section>
+    </>
+  )
+}
+
+export default Example
